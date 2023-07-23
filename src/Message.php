fe17743f0f65597e634bf00ecff929046a9883f9
Weida <sgenmi@gmail.com>
2023-07-23 22:34:34 +0800

    消息类型定义

--- src/Message.php
+++ src/Message.php
@@ -10,6 +10,50 @@ namespace Weida\WeixinCore;
 
 class Message
 {
-    
+    //接受消息类型
+    const TYPE_TEXT = "text";
+    const TYPE_IMAGE = "image";
+    const TYPE_VOICE = 'voice';
+    const TYPE_VIDEO = 'video';
+    /**
+     * @var string 被动回复时
+     */
+    const TYPE_MUSIC = 'music';
+    const TYPE_SHORTVIDEO = 'shortvideo';
+    const TYPE_LOCATION = 'location';
+    const TYPE_LINK = 'link';
+    /**
+     * @var string 被动回复时图文
+     */
+    const TYPE_NEWS = 'news'; //
+
+    //授受事件类型
+    const EVENT_SUBSCRIBE = 'subscribe';
+    const EVENT_UNSUBSCRIBE = 'unsubscribe';
+    const EVENT_SCAN = 'scan';
+    const EVENT_LOCATION = 'location';
+    const EVENT_CLICK = 'click';
+    const EVENT_VIEW = 'view';
+    const EVENT_TEMPLATESENDJOBFINISH = 'templatesendjobfinish';
+
+
+    /**
+     * @param string $msgType
+     * @param string $event
+     * @return string|null
+     * @author Sgenmi
+     */
+    public static function getTypeVal(string $msgType,string $event=''):?string{
+        if($msgType=='event'){
+           $_defined = strtoupper($msgType.'_'.$event);
+        }else{
+            $_defined = strtoupper('type_'.$msgType);
+        }
+        $localConst = 'self::'.$_defined;
+        if(!defined($localConst)){
+            return null;
+        }
+        return strval(constant($localConst));
+    }
 
 }
