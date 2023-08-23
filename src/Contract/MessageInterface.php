622ed471998713beddd77fb0e74e0d4c8b51f0c0
Weida <sgenmi@gmail.com>
2023-08-24 00:24:01 +0800

    add:消息接口

---
+++ src/Contract/MessageInterface.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+/**
+ * Author: Weida
+ * Date: 2023/8/23 22:43
+ * Email: sgenmi@gmail.com
+ */
+
+namespace Weida\WeixinCore\Contract;
+
+interface MessageInterface
+{
+    public function setAttributes(array|string $attributes):void;
+    public function geAttributes():array;
+}
